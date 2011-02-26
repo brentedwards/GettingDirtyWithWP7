d0b118a5b6e3f795e3c8299ecf33598da60463c8
brentedwards <brent.l.edwards@gmail.com>
2011-02-26 01:08:30 -0600

    Beginning to update unit tests.

---
+++ GettingDirty.Core.Tests/Converters/BoolToFontStyleConverterTests.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Ink;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+using Microsoft.Silverlight.Testing;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using GettingDirty.Core.Converters;
+
+namespace GettingDirty.Core.Tests.Converters
+{
+	[TestClass]
+	public class BoolToFontStyleConverterTests : SilverlightTest
+	{
+		[TestMethod]
+		public void Convert_True()
+		{
+			var expectedStyle = FontStyles.Italic;
+
+			var converter = new BoolToFontStyleConverter();
+			converter.CompletedStyle = expectedStyle;
+
+			var actualStyle = converter.Convert(true, null, null, null);
+
+			Assert.AreEqual(expectedStyle, actualStyle);
+		}
+
+		[TestMethod]
+		public void Convert_False()
+		{
+			var expectedStyle = FontStyles.Normal;
+
+			var converter = new BoolToFontStyleConverter();
+
+			var actualStyle = converter.Convert(false, null, null, null);
+
+			Assert.AreEqual(expectedStyle, actualStyle);
+		}
+
+		[TestMethod]
+		public void Convert_NotBoolean()
+		{
+			var expectedStyle = FontStyles.Normal;
+
+			var converter = new BoolToFontStyleConverter();
+
+			var actualStyle = converter.Convert(new object(), null, null, null);
+
+			Assert.AreEqual(expectedStyle, actualStyle);
+		}
+	}
+}
---
+++ GettingDirty.Core.Tests/Converters/BoolToVisibilityConverterTests.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Ink;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+using Microsoft.Silverlight.Testing;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using GettingDirty.Core.Converters;
+
+namespace GettingDirty.Core.Tests.Converters
+{
+	[TestClass]
+	public class BoolToVisibilityConverterTests : SilverlightTest
+	{
+		[TestMethod]
+		public void Convert_True()
+		{
+			var expectedVisibility = Visibility.Visible;
+
+			var converter = new BoolToVisibilityConverter();
+
+			var actualVisibility = converter.Convert(true, null, null, null);
+
+			Assert.AreEqual(expectedVisibility, actualVisibility);
+		}
+
+		[TestMethod]
+		public void Convert_False()
+		{
+			var expectedVisibility = Visibility.Collapsed;
+
+			var converter = new BoolToVisibilityConverter();
+
+			var actualVisibility = converter.Convert(false, null, null, null);
+
+			Assert.AreEqual(expectedVisibility, actualVisibility);
+		}
+
+		[TestMethod]
+		public void Convert_NotBool()
+		{
+			var expectedVisibility = Visibility.Visible;
+
+			var converter = new BoolToVisibilityConverter();
+
+			var actualVisibility = converter.Convert(new object(), null, null, null);
+
+			Assert.AreEqual(expectedVisibility, actualVisibility);
+		}
+	}
+}
