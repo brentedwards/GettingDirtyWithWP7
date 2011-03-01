60434df28ce8e9fe3d57cbe8e12212b84bd9e142
brentedwards <brent.l.edwards@gmail.com>
2011-02-28 22:47:01 -0600

    Emptying out ViewModelLocator.

--- GettingDirty.Core/ViewModels/ViewModelLocator.cs
+++ GettingDirty.Core/ViewModels/ViewModelLocator.cs
@@ -5,46 +5,6 @@ namespace GettingDirty.Core.ViewModels
 {
 	public class ViewModelLocator
 	{
-		private IMainViewModel _mainViewModel;
-		public IMainViewModel MainViewModel
-		{
-			get
-			{
-				if (_mainViewModel == null)
-				{
-					if (IsInDesignMode)
-					{
-						_mainViewModel = new DesignMainViewModel();
-					}
-					else
-					{
-						_mainViewModel = Ioc.Container.Resolve<IMainViewModel>();
-					}
-				}
-				return _mainViewModel;
-			}
-		}
-
-		private IDetailsViewModel _detailsViewModel;
-		public IDetailsViewModel DetailsViewModel
-		{
-			get
-			{
-				if (_detailsViewModel == null)
-				{
-					if (IsInDesignMode)
-					{
-						_detailsViewModel = new DesignDetailsViewModel();
-					}
-					else
-					{
-						_detailsViewModel = Ioc.Container.Resolve<IDetailsViewModel>();
-					}
-				}
-				return _detailsViewModel;
-			}
-		}
-
 		private bool IsInDesignMode
 		{
 			get { return DesignerProperties.IsInDesignTool; }
