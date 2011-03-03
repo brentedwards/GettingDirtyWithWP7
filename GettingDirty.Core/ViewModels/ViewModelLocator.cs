b6eaf1341f76fb6239a807c1858ce28f9bc24f03
brentedwards <brent.l.edwards@gmail.com>
2011-03-02 22:17:25 -0600

    More presentation prep.

--- GettingDirty.Core/ViewModels/ViewModelLocator.cs
+++ GettingDirty.Core/ViewModels/ViewModelLocator.cs
@@ -5,6 +5,10 @@ namespace GettingDirty.Core.ViewModels
 {
 	public class ViewModelLocator
 	{
+		// TODO: 1. MainViewModel
+
+		// TODO: 2. DetailsViewModel
+
 		private bool IsInDesignMode
 		{
 			get { return DesignerProperties.IsInDesignTool; }
