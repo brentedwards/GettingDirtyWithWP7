f68714b3ca1b4ec4b0da44a7da87e70e530c6d3b
brentedwards <brent.l.edwards@gmail.com>
2011-04-06 22:10:19 -0500

    Prepping for MainPage Design stage.

--- GettingDirty.Core/ViewModels/ViewModelLocator.cs
+++ GettingDirty.Core/ViewModels/ViewModelLocator.cs
@@ -5,7 +5,25 @@ namespace GettingDirty.Core.ViewModels
 {
 	public class ViewModelLocator
 	{
-		// TODO: 1. MainViewModel
+		private MainViewModel _mainViewModel;
+		public MainViewModel MainViewModel
+		{
+			get
+			{
+				if (_mainViewModel == null)
+				{
+					if (IsInDesignMode)
+					{
+						_mainViewModel = new DesignMainViewModel();
+					}
+					else
+					{
+						_mainViewModel = Ioc.Container.Resolve<MainViewModel>();
+					}
+				}
+				return _mainViewModel;
+			}
+		}
 
 		// TODO: 2. DetailsViewModel
 
