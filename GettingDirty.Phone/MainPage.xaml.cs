974bd6d888a8defd10dbe751c4568979d1fea0f9
brentedwards <brent.l.edwards@gmail.com>
2011-04-07 21:55:46 -0500

    Prepping for Isolated Storage phase.

--- GettingDirty.Phone/MainPage.xaml.cs
+++ GettingDirty.Phone/MainPage.xaml.cs
@@ -30,13 +30,40 @@ namespace GettingDirty.Phone
 			ViewModel = DataContext as MainViewModel;
 		}
 
-		// TODO: 2. OnNavigatedFrom
+		protected override void OnNavigatedFrom(System.Windows.Navigation.NavigationEventArgs e)
+		{
+			// Save state
+			State[KEY_TITLE] = NewTaskTitle.Text;
+			State[KEY_PRIORITY] = NewTaskPriority.SelectedItem;
 
-		// TODO: 3. OnNavigatedTo
+			// Save tasks
+			ViewModel.SaveTasks();
+
+			base.OnNavigatedFrom(e);
+		}
+
+		protected override void OnNavigatedTo(System.Windows.Navigation.NavigationEventArgs e)
+		{
+			base.OnNavigatedTo(e);
+
+			// Restore state, if it exists
+			object value;
+			if (State.TryGetValue(KEY_TITLE, out value))
+			{
+				NewTaskTitle.Text = value.ToString();
+			}
+
+			if (State.TryGetValue(KEY_PRIORITY, out value))
+			{
+				NewTaskPriority.SelectedItem = value;
+			}
+
+			ViewModel.LoadTasks();
+		}
 
 		private void NewTask_Click(object sender, EventArgs e)
 		{
-			// TODO: 1. Navigate
+			NavigationService.Navigate(new Uri("/Views/DetailsView.xaml", UriKind.Relative));
 		}
 	}
 }
